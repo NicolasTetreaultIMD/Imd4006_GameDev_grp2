62429bc82b6566c3d6d10a7c1d30191fdd39691a
Ponther <99770144+Ponther@users.noreply.github.com>
2024-11-20 16:38:19 -0500

    Cinemachines be workin

---
+++ CharacterController_Prototype/Assets/CameraHolders.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHolders : MonoBehaviour
+{
+    public GameObject carCamera;
+    public GameObject carCinemachine;
+}
--- CharacterController_Prototype/Assets/MultiplayerManager.cs
+++ CharacterController_Prototype/Assets/MultiplayerManager.cs
@@ -1,11 +1,25 @@
 using Cinemachine;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MultiplayerManager : MonoBehaviour
 {
     public List<PlayerInput> players = new List<PlayerInput>();
+
+    LayerMask p1OcclusionMask;
+    LayerMask p2OcclusionMask;
+    LayerMask p3OcclusionMask;
+    LayerMask p4OcclusionMask;
+
+    private void Start()
+    {
+        p1OcclusionMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Item", "Water", "UI", "Pole", "Obstacle", "Vehicle", "Ignore", "Player1");
+        p2OcclusionMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Item", "Water", "UI", "Pole", "Obstacle", "Vehicle", "Ignore", "Player2");
+        p3OcclusionMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Item", "Water", "UI", "Pole", "Obstacle", "Vehicle", "Ignore", "Player3");
+        p4OcclusionMask = LayerMask.GetMask("Default", "TransparentFX", "Ignore Raycast", "Item", "Water", "UI", "Pole", "Obstacle", "Vehicle", "Ignore", "Player4");
+    }
 
     private void OnEnable()
     {
@@ -22,6 +36,11 @@ public class MultiplayerManager : MonoBehaviour
     private void OnPlayerJoined(PlayerInput playerInput)
     {
         players.Add(playerInput);
+
+        GameObject player = playerInput.gameObject;
+
+        player = cullingMaskApplier(player, playerInput.playerIndex);
+
         Debug.Log($"Player joined: {playerInput.playerIndex}");
     }
 
@@ -29,5 +48,42 @@ public class MultiplayerManager : MonoBehaviour
     {
         players.Remove(playerInput);
         Debug.Log($"Player left: {playerInput.playerIndex}");
+    }
+
+    private GameObject cullingMaskApplier(GameObject player, int playerId)
+    {
+
+        GameObject playerCam = player.GetComponent<CameraHolders>().carCamera;
+        GameObject playerCine = player.GetComponent<CameraHolders>().carCinemachine;
+
+        if (playerId == 0)
+        {
+            playerCam.GetComponent<Camera>().cullingMask = p1OcclusionMask;
+            playerCam.layer = 10;
+            playerCine.layer = 10;
+        }
+
+        if(playerId == 1) 
+        {
+            playerCam.GetComponent<Camera>().cullingMask = p2OcclusionMask;
+            playerCam.layer = 11;
+            playerCine.layer = 11;
+        }
+
+        if (playerId == 2)
+        {
+            playerCam.GetComponent<Camera>().cullingMask = p3OcclusionMask;
+            playerCam.layer = 12;
+            playerCine.layer = 12;
+        }
+
+        if (playerId == 3)
+        {
+            playerCam.GetComponent<Camera>().cullingMask = p4OcclusionMask;
+            playerCam.layer = 14;
+            playerCine.layer = 14;
+        }
+
+        return player;
     }
 }
