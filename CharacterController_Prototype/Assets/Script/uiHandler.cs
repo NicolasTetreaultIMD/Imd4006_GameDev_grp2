2b90e4af8655b8fca0bfe8835eea12ae28cf84a2
Everett Ohman <37959042+Evo406@users.noreply.github.com>
2024-11-25 20:11:29 -0500

    added bomb UI

--- CharacterController_Prototype/Assets/Script/uiHandler.cs
+++ CharacterController_Prototype/Assets/Script/uiHandler.cs
@@ -9,19 +9,50 @@ public class uiHandler : MonoBehaviour
 {
     public GameObject[] players = new GameObject[4];
 
-    [Header("UI Elements")]
-    public TextMeshProUGUI p1_ammoCount;
-    public TextMeshProUGUI p0_ammoCount;
+    [Header("Ammo Types")]
+    public GameObject p0_currentBomb;
+    public GameObject p0_currentMine;
+    public GameObject p0_currentNuke;
+
+    public GameObject p0_nextBomb;
+    public GameObject p0_nextMine;
+    public GameObject p0_nextNuke;
+
+    public GameObject p1_currentBomb;
+    public GameObject p1_currentMine;
+    public GameObject p1_currentNuke;
+
+    public GameObject p1_nextBomb;
+    public GameObject p1_nextMine;
+    public GameObject p1_nextNuke;
+
+    [Header("Ammo Types")]
+    public GameObject p0_health3;
+    public GameObject p0_health2;
+    public GameObject p0_health1;
+
+    public GameObject p1_health3;
+    public GameObject p1_health2;
+    public GameObject p1_health1;
+
 
     // Start is called before the first frame update
 
     void Start()
     {
-        p0_ammoCount = FindObjectOfType<TextMeshProUGUI>();
-        p1_ammoCount = FindObjectOfType<TextMeshProUGUI>();
 
-        p0_ammoCount.text = "Ammo: 0";
-        p1_ammoCount.text = "Ammo: 0";
+        p0_currentBomb.SetActive(false);
+        p0_currentMine.SetActive(false);
+        p0_currentNuke.SetActive(false);
+        p0_nextBomb.SetActive(false);
+        p0_nextMine.SetActive(false);
+        p0_nextNuke.SetActive(false);
+        p1_currentBomb.SetActive(false);
+        p1_currentMine.SetActive(false);
+        p1_currentNuke.SetActive(false);
+        p1_nextBomb.SetActive(false);
+        p1_nextMine.SetActive(false);
+        p1_nextNuke.SetActive(false);
 
     }
 
@@ -31,15 +62,155 @@ public class uiHandler : MonoBehaviour
         // Update the UI text to show the projectile count
         if (players[0] != null)
         {
-            p0_ammoCount.text = "Ammo: " + players[0].GetComponent<CarController>().cannon.projectile.Count.ToString();
+
+            // PLAYER 1 has AMMO
+            if (players[0].GetComponent<CarController>().cannon.projectile.Count > 0) 
+            {
+                ShowCurrentAmmoType(0, players[0].GetComponent<CarController>().cannon.projectile[0].name); // Show current ammo type
+                ShowNextAmmoType(0, players[0].GetComponent<CarController>().cannon.projectile[1].name); // Show current ammo type
+            }
+            else // PLAYER 1 has no ammo
+            {
+                ShowCurrentAmmoType(0, "Empty");
+            }
         }
 
         if (players[1] != null) 
         {
-            p1_ammoCount.text = "Ammo: " + players[1].GetComponent<CarController>().cannon.projectile.Count.ToString();
+            // PLAYER 2 has AMMO
+            if (players[1].GetComponent<CarController>().cannon.projectile.Count > 0)
+            {
+                ShowCurrentAmmoType(1, players[1].GetComponent<CarController>().cannon.projectile[0].name); // Show current ammo type
+                ShowNextAmmoType(1, players[1].GetComponent<CarController>().cannon.projectile[1].name); // Show current ammo type
+            }
+            else // PLAYER 2 has no ammo
+            {
+                ShowCurrentAmmoType(1, "Empty");
+            }
 
-            //Debug.Log(players[1].GetComponent<CarController>().health);
+        }
+    }
 
+    // Function toggles element visibility state using the bool type
+    public void ShowCurrentAmmoType(int playerID, string type) 
+    {
+        if (playerID == 0) // FIRST PLAYER
+        {
+            if (type == "Bomb Item")
+            {
+                p0_currentBomb.SetActive(true);
+                p0_currentMine.SetActive(false);
+                p0_currentNuke.SetActive(false);
+            }
+            else if (type == "Mine Item")
+            {
+                p0_currentBomb.SetActive(false);
+                p0_currentMine.SetActive(true);
+                p0_currentNuke.SetActive(false);
+            }
+            else if (type == "Nuke Item")
+            {
+                p0_currentBomb.SetActive(false);
+                p0_currentMine.SetActive(false);
+                p0_currentNuke.SetActive(true);
+
+            }
+            else if (type == "Empty") // No ammo is left
+            {
+                p0_currentBomb.SetActive(false);
+                p0_currentMine.SetActive(false);
+                p0_currentNuke.SetActive(false);
+            }
+        }
+        else if (playerID == 1) // SECOND PLAYER
+        {
+            if (type == "Bomb Item")
+            {
+                p1_currentBomb.SetActive(true);
+                p1_currentMine.SetActive(false);
+                p1_currentNuke.SetActive(false);
+            }
+            else if (type == "Mine Item")
+            {
+                p1_currentBomb.SetActive(false);
+                p1_currentMine.SetActive(true);
+                p1_currentNuke.SetActive(false);
+            }
+            else if (type == "Nuke Item")
+            {
+                p1_currentBomb.SetActive(false);
+                p1_currentMine.SetActive(false);
+                p1_currentNuke.SetActive(true);
+
+            }
+            else if (type == "Empty") // No ammo is left
+            {
+                p1_currentBomb.SetActive(false);
+                p1_currentMine.SetActive(false);
+                p1_currentNuke.SetActive(false);
+            }
+        }
+    }
+
+    // Function toggles element visibility state using the bool type
+    public void ShowNextAmmoType(int playerID, string type)
+    {
+        if (playerID == 0) // PLAYER ONE
+        {
+            if (type == "Bomb Item")
+            {
+                p0_currentBomb.SetActive(true);
+                p0_currentMine.SetActive(false);
+                p0_currentNuke.SetActive(false);
+            }
+            else if (type == "Mine Item")
+            {
+                p0_currentBomb.SetActive(false);
+                p0_currentMine.SetActive(true);
+                p0_currentNuke.SetActive(false);
+            }
+            else if (type == "Nuke Item")
+            {
+                p0_currentBomb.SetActive(false);
+                p0_currentMine.SetActive(false);
+                p0_currentNuke.SetActive(true);
+
+            }
+            else if (type == "Empty") // No ammo is left
+            {
+                p0_currentBomb.SetActive(false);
+                p0_currentMine.SetActive(false);
+                p0_currentNuke.SetActive(false);
+            }
+        }
+
+        if (playerID == 1) // PLAYER 2
+        {
+            if (type == "Bomb Item")
+            {
+                p1_currentBomb.SetActive(true);
+                p1_currentMine.SetActive(false);
+                p1_currentNuke.SetActive(false);
+            }
+            else if (type == "Mine Item")
+            {
+                p1_currentBomb.SetActive(false);
+                p1_currentMine.SetActive(true);
+                p1_currentNuke.SetActive(false);
+            }
+            else if (type == "Nuke Item")
+            {
+                p1_currentBomb.SetActive(false);
+                p1_currentMine.SetActive(false);
+                p1_currentNuke.SetActive(true);
+
+            }
+            else if (type == "Empty") // No ammo is left
+            {
+                p1_currentBomb.SetActive(false);
+                p1_currentMine.SetActive(false);
+                p1_currentNuke.SetActive(false);
+            }
         }
     }
 
