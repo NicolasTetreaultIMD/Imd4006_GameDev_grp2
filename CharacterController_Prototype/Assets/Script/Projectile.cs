67d4bc8843d0f0fdba0e8e16829d52307d6525e3
Everett Ohman <37959042+Evo406@users.noreply.github.com>
2024-11-26 23:51:33 -0500

    Revert "Merge pull request #100 from NicolasTetreaultIMD/everetts-branch"
    This reverts commit c049372651e1eeb5534be52a920eba0356437e1a, reversing
    changes made to 9cfc0e99e7cbff88a712b8ff3d4df88146b1d6f8.

--- CharacterController_Prototype/Assets/Script/Projectile.cs
+++ CharacterController_Prototype/Assets/Script/Projectile.cs
@@ -36,6 +36,7 @@ public class Projectile : MonoBehaviour
         madeContact = false;
         mass = gameObject.GetComponent<Rigidbody>().mass;
 
+        
     }
 
     // Update is called once per frame
@@ -77,16 +78,6 @@ public class Projectile : MonoBehaviour
         carController = newCart;
         forcesApplied = true;
 
-        if (gameObject.tag == "Mine")
-        {
-            carController.audioHandler.mineBeep();
-        }
-
-        if (gameObject.tag == "Nuke")
-        {
-            carController.audioHandler.nukeWhistle();
-        }
-
     }
 
 
@@ -104,7 +95,7 @@ public class Projectile : MonoBehaviour
                 explosion.GetComponent<DamageApplier>().playerId = carController.playerId;
                 bombMesh.enabled = false;
 
-                carController.audioHandler.impactExplosion(); // PLAY Audio
+                carController.audioHandler.impactExplosion();
                 haptics.ExplosionHaptics();
 
                 StartCoroutine(FadeOut());
@@ -161,8 +152,6 @@ public class Projectile : MonoBehaviour
             if (other.gameObject.tag == "Player" && madeContact == true)
             {
                 explosion.gameObject.SetActive(true);
-                carController.audioHandler.impactExplosion(); // play explosion SFX
-
             }
         }
 
@@ -171,7 +160,6 @@ public class Projectile : MonoBehaviour
             if (other.gameObject.tag == "Player" && madeContact == true)
             {
                 explosion.gameObject.SetActive(true);
-                carController.audioHandler.activateTrap();
             }
         }
     }
